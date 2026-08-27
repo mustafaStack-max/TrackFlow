67345432c463ebbb23312e47e42031869c965bf5
mustafaStack-max <mustafaboujenoui@gmail.com>
2026-08-27 10:14:41 +0100

    Add Requirest Account  validation rules

---
+++ app/Http/Requests/StoreAccountRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => ['string' , 'required' , 'max:100' , 'unique:accounts,name'] ,
+            "uuid" => ['string' ] ,
+            "type" => ['string' , 'required'  , Rule::in(['cash','bank','card','savings','other'])] ,
+            'balance'=> ['numeric' , 'required'] ,
+            'currency' =>  [ 'nullable' , 'string' , 'size:3'] ,
+            'color_hex' => ['nullable' , 'string' , 'hex_color'] ,
+            'is_active' => [ 'nullable' , 'boolean' ]
+        ];
+    }
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
 
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
+            $table->softDeletes();  
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
