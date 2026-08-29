9c27253416b2a210be032c26910480c887b40fae
mustafaStack-max <mustafaboujenoui@gmail.com>
2026-08-29 18:59:45 +0100

    add Transaction Dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -16,9 +18,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -26,14 +26,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/accounts' , [AccountController::class , 'index'])->middleware('auth') ;
+Route::get('/accounts' , [AccountController::class , 'index'])->middleware('auth')->name('accounts.index') ;
 Route::post('/accounts' , [AccountController::class , 'store'])->middleware('auth')->name('accounts.store') ;
 Route::put('/accounts/{account:uuid}' , [AccountController::class , 'update'])->middleware('auth')->name('accounts.update') ;
 Route::delete('/accounts/{account:id}' , [AccountController::class , 'destroy'])->middleware('auth')->name('accounts.destroy') ;
 
-Route::get('/categories' , [CategoryController::class , 'index'])->middleware('auth') ;
+Route::get('/categories' , [CategoryController::class , 'index'])->middleware('auth')->name('categories.index') ;
 Route::post('/categories' , [CategoryController::class , 'store'])->middleware('auth')->name('categories.store') ;
 Route::put('/categories/{category:id}' , [CategoryController::class , 'update'])->middleware('auth')->name('categories.update')  ;
 Route::delete('/categories/{category:id}' , [CategoryController::class , 'destroy'])->middleware('auth')->name('categories.destroy')  ;
+
+
+Route::get('/transactions' , [TransactionController::class , 'index'])->middleware('auth')->name('transactions.index') ;
+Route::post('/transactions' , [TransactionController::class , 'store'])->middleware('auth')->name('transactions.store') ;
+Route::put('/transactions/{transaction:id}' , [TransactionController::class , 'update'])->middleware('auth')->name('transactions.update')  ;
+Route::delete('/transactions/{transaction:id}' , [TransactionController::class , 'destroy'])->middleware('auth')->name('transactions.destroy')  ;
+
 
 require __DIR__.'/auth.php';
